3369838ff797088eb004d06278073ba64731884d
Nikolay <efimenko-01@mail.ru>
2020-08-18 10:56:07 +0600

    sending weather data to vk

--- weatherparse.py
+++ weatherparse.py
@@ -1,8 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
 import config
 import json
-
+import vk_api
+from vk_api.utils import get_random_id
 city = config.city
 api_key = config.api_key
 
@@ -18,3 +18,29 @@ data = req.json()
 print(data)
 with open('dump.json', 'w') as jf:
     json.dump(data , jf)
+
+email = config.email
+password = config.password 
+session = requests.Session()
+session.verify = False
+
+vk_session = vk_api.VkApi(login=email, password=config.password)
+vk_session.auth(token_only=True)
+print(vk_session.token)
+
+vk = vk_session.get_api()
+print(vk.account.getProfileInfo())
+print(vk.account.getAppPermissions())
+try:
+    vk.messages.send(user_id= config.userid, message="{}".format(data),random_id=get_random_id())
+except Exception:
+    print('Нет доступа к сообщениям')
+    vk.friends.add(user_id=config.userid)
+
+#res = session.post("https://vk.com/login", data={'email' : email , 'password' : password})
+#res.raise_for_status()
+
+#print(res.status_code)
+
+#res = session.get("https://vk.com/feed")
+
